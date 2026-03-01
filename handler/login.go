3b40a6b0b8b80b4b23f2eaa08866f59dbfa4831a
whitekotan1 <whitekotan1@gmail.com>
2026-03-01 22:57:35 +0200

    feat: add auth for admin + cookie + frontend

--- handler/login.go
+++ handler/login.go
@@ -16,5 +16,34 @@ func (h *PasswordHandler) LoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	case http.MethodPost:
 
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		inputedPassword := r.FormValue("password")
+
+		if inputedPassword != h.Password {
+			http.Error(w, "invalid password", http.StatusUnauthorized)
+			return
+		}
+
+		cookie := &http.Cookie{
+			Name:     "formus_auth",
+			Value:    "authorized_true",
+			Path:     "/form",
+			MaxAge:   3600 * 200,
+			HttpOnly: true,
+			Secure:   false,
+			SameSite: http.SameSiteLaxMode,
+		}
+
+		http.SetCookie(w, cookie)
+		http.Redirect(w, r, "/adminPanel", http.StatusFound)
+
+	default:
+		http.Error(w, "method not aloowed", http.StatusMethodNotAllowed)
+
 	}
 }
