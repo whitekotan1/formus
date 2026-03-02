cfc9952a63227aaf525da47118a6965a93b8820c
whitekotan1 <whitekotan1@gmail.com>
2026-03-02 21:38:11 +0200

    feat: add to FormHandler handle logic

--- handler/form.go
+++ handler/form.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
+	"formus/database"
 	"net/http"
 )
 
@@ -10,4 +13,29 @@ func FormHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "only post method allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	formID := r.URL.Query().Get("form_id")
+
+	if formID == "" {
+		http.Error(w, "invalid form_id", http.StatusBadRequest)
+		return
+	}
+
+	jsonData, err := json.Marshal(r.PostForm)
+
+	if err != nil {
+		http.Error(w, "can't encode data", http.StatusInternalServerError)
+		return
+	}
+
+	err = database.SaveRequest(formID, jsonData)
+
+	if err != nil {
+		fmt.Println("can't save form", err)
+		return
+	}
+	fmt.Println("form was saved")
+
+	w.WriteHeader(http.StatusOK)
+
 }
